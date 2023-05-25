60aa95f43517731d61cc3613d1b6494824e249ee
vitaly <vitaly.belsky@startmatter.com>
2023-05-25 16:56:33 +0300

    Update project settings

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 110,
+  tabWidth: 2,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
